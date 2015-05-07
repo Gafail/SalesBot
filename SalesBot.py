4d7b3222acfad546c5b1d9df7cf48f1f4e623b89
Rusty45 <b-man542@hotmail.com>
2015-05-07 16:18:03 +1200

    Added "!Interested" feature, added new database table "interested", added more comments, fixed a minor database issue with price, added input conversion for the hub escaped character "$". \\ Bugs: needs user input checking

--- SalesBot.py
+++ SalesBot.py
@@ -12,23 +12,36 @@ import time
 # people can want sell and list an interest in an item
 
 def funForsale(self,msg,user):
-    """Lists items currently in the database that are for sale"""
+    """Lists items currently in the database that are for sale and how many users are interested in them"""
     c = conn.cursor()
     info = "Items currently for sale:\n"
     messages = 0
-    for row in c.execute('SELECT * FROM sales ORDER BY ID'):
+    c.execute('SELECT * FROM sales ORDER BY ID')
+    sales_table = c.fetchall()
+    for row in sales_table:
         
-        ID , USER, ITEM, PRICE, INTEREST = row
-        if PRICE == "None":
-            info += "\n" + "ID:" + str(ID).ljust(4) + " " + str(USER) + ":".ljust(10) + str(ITEM)
+        ID , USER, ITEM, PRICE = row
+        c.execute('SELECT SALES_ID, USER FROM interested WHERE SALES_ID = ? ORDER BY INTERESTED_ID', (ID,))
+        item_interest = c.fetchall()
+        #if PRICE == "None":
+        if user == USER:
+            info += "\n" + format(("ID:" + str(ID)),"<9")+ format((str(USER) + ":"),"<20") + str(ITEM).strip() + "\n" + format("Interested User(s): ",">32")
+            if item_interest != []:
+                for item in item_interest:
+                    _,name = item
+                    info += " " + name
+            else:
+                info += " None"
         else:
-            info += "\n" + "ID:" + str(ID).ljust(4) + " " + str(USER) + ":".ljust(10) + str(ITEM) + " $" + str(PRICE)
+            info += "\n" + format(("ID:" + str(ID)),"<9")+ format((str(USER) + ":"),"<20") + format(str(ITEM).strip(),"<200") + "\n" + format("Interested User(s): ",">32") +  str(len(item_interest))
+        #else:
+            #info += "\nID:" + str(ID).ljust(4)  + " " + str(USER) + ":".ljust(10) + str(ITEM).strip() + " $" + str(PRICE)
          
         if messages < 8:
             messages += 1
         else:
             self.pm(user,info)
-            info = "\n"
+            info = ""
             messages = 0
     
     info += "\n"        
@@ -91,28 +104,26 @@ def funNotwanted(self,msg,user):
     conn.commit()  
     return True 
     
-    
-    
 def funSell(self,msg,user):
     """Adds an item to the database of items for sale (!sell <Item> <Price>)"""
     Item = " "
     Price = " "
     c = conn.cursor()
-    ssellingwant = msg.split("!sell")[1].strip()
+    ssellingwant = msg.split("!sell")[1].strip().replace("&#36;","$")
     
-    if "&#36;" not in msg.lower():
+    if "$" not in msg.lower():
         Price = "None"
         Item = ssellingwant
     else:
-        Item,Price = ssellingwant.rsplit("&#36;",1)
-    interest = "[]"
+        Item,Price = ssellingwant.rsplit("$;",1)
+        ## Temporay fix -Rusty
+        Item = ssellingwant
+        ##
     # Insert a row of data
-    Newline = user, Item, Price, interest
-    c.execute("INSERT INTO sales(USER, ITEM, PRICE, INTEREST) VALUES (?,?,?,?)",Newline)      
+    Newline = user, Item, Price
+    c.execute("INSERT INTO sales(USER, ITEM, PRICE) VALUES (?,?,?)",Newline)      
     conn.commit()    
-    return True
-
-    
+    return True   
     
 def funSold(self,msg,user):
     """removes an item for sale from the database (!Sold <ID>)"""
@@ -128,12 +139,9 @@ def funInterested(self,msg,user):
     to a list which only the seller can see"""
     ID = msg.split("!interested")[1].strip()
     c = conn.cursor()
-    interestList = c.execute('SELECT INTEREST FROM sales WHERE ID = ?',(ID,))
-    interestList.append(user)
-    c.execute("INSERT INTO sales(INTEREST) WHERE ID = ? VALUES (?)",(ID,interestList))      
+    c.execute("INSERT INTO interested(SALES_ID, USER) VALUES (?,?)",(ID,user))      
     conn.commit() 
     return True
-
  
 class SalesBot(PyDC):
 
@@ -149,6 +157,7 @@ class SalesBot(PyDC):
     #password='123321'
     
     class Sales_Responce(object):
+        """ Must be a child of PyDC. Is a object which stores how to respond to a command"""
         def __init__(self):
             self.cmd = ""
             self.msg_respond = True     #Will respond to public message
@@ -164,7 +173,7 @@ class SalesBot(PyDC):
             else:
                 if self.msg_respond:
                     parent.say(self.msg_reply)
-            print "Replied to", user
+            print "Replied to", user + " (" + self.cmd + ")"
             return
 
     def __init__(self):
@@ -229,16 +238,22 @@ class SalesBot(PyDC):
         forsale.pmsg_reply = "If you wish to buy something PM the user and list your interest with !interested <ID>"
         forsale.cmd_action = funForsale
         add_cmd(forsale)
-   
 
+        interested = self.Sales_Responce()
+        interested.cmd = "!interested"
+        interested.msg_respond = False     
+        interested.msg_reply = "Your Interest has been recoreded"
+        interested.pmsg_reply = "Your Interest has been recoreded"
+        interested.cmd_action = funInterested
+        add_cmd(interested)
+   
     def messageReply(self, user, msg, pm):
         """ if the message needs to be replied to then here is where the
         messages are. If pm = true then private message else public message"""
-        #print msg
-        cmd = msg.split(' ')[0]
-        if cmd in self.COMMANDS.keys():
-            if self.COMMANDS[cmd].cmd_action(self,msg,user):
-                self.COMMANDS[cmd].reply(self,pm,user)
+        cmd = msg.split(' ')[0]                              #grab the command from the start ["!cmd","message"]
+        if cmd in self.COMMANDS.keys():                      #is the command valid
+            if self.COMMANDS[cmd].cmd_action(self,msg,user): #exicute the action and if true reply
+                self.COMMANDS[cmd].reply(self,pm,user)       #reply to user
     
         output = ''
         respond = False
@@ -285,9 +300,6 @@ class SalesBot(PyDC):
         if respond:
             self.say(output)        
              
-    
-    
-    
     def onPrivate(self, user, msg):
         self.messageReply(user, msg, True) #replys to a private message
               
@@ -302,8 +314,8 @@ if not os.path.isfile('HubSales.db'):
     conn = sqlite3.connect('HubSales.db')
     c = conn.cursor()
     # Create table
-    c.execute('''CREATE TABLE sales (ID INTEGER PRIMARY KEY AUTOINCREMENT , USER, ITEM, PRICE, INTEREST)''')
-    
+    c.execute('''CREATE TABLE sales (ID INTEGER PRIMARY KEY AUTOINCREMENT , USER, ITEM, PRICE)''')
+    c.execute('''CREATE TABLE interested (INTERESTED_ID INTEGER PRIMARY KEY, SALES_ID INTEGER, USER TEXT, FOREIGN KEY(SALES_ID) REFERENCES sales(ID))''')
     c.execute('''CREATE TABLE wanting (ID INTEGER PRIMARY KEY AUTOINCREMENT , USER, ITEM)''')
 else:
     conn = sqlite3.connect('HubSales.db')
